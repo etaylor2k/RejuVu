83230ffd26d97e1f8e140b5e2a8be6cca6dbcef3
Endris Taylor <etaylor2k@gmail.com>
2011-08-16 21:03:29 -0500

    implimentation of model.user_levels

--- rejuvu/model/user_level.py
+++ rejuvu/model/user_level.py
@@ -1,4 +1,20 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
+from sqlalchemy import Column
+from sqlalchemy.types inport String, Text
+from sqlalchemy.dialects.postgresql import *
 
+from rejuvu.model.meta import Base
 
+__author__ = "Endris Taylor"
+class UserLevel(Base):
+    __tablename__ = "user_levels"
+
+    ulid = Column('ulid', INTEGER, primary_key=True)
+    name =Column('name', VARCHAR(255))
+
+    def __init__(self, name =''):
+    # Constructor
+        self.name = name
+
+    def __repr__(self):
+    # Representaion
+        return "<UserLevel('%s')" %self.name
